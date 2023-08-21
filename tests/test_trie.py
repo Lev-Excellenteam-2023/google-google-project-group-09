e7c94bfe2315393a4bfadf7e937acafcf3e46ff7
rivkaadler1 <adlerrivka66@gmail.com>
2023-08-21 13:57:02 +0300

    Implement Trie data structure and text processing

---
+++ tests/test_trie.py
@@ -0,0 +1,47 @@
+import unittest
+import sentence_completion
+
+
+
+class TestTrie(unittest.TestCase):
+    def setUp(self):
+        self.trie = trie_ds.Trie()
+
+    def test_insert_and_search(self):
+        self.trie.insert("apple", ("file1.txt", 1, 1))
+        self.trie.insert("banana", ("file1.txt", 1, 2))
+        self.trie.insert("cherry", ("file1.txt", 2, 1))
+        self.trie.insert("apple", ("file2.txt", 1, 1))
+
+        locations_apple = self.trie.search("app")
+        locations_banana = self.trie.search("ban")
+        locations_grape = self.trie.search("grape")
+
+        self.assertEqual(locations_apple, {
+            "file1.txt": [(1, 1), (1, 1)],
+            "file2.txt": [(1, 1)]
+        })
+        self.assertEqual(locations_banana, {
+            "file1.txt": [(1, 2)]
+        })
+        self.assertEqual(locations_grape, {})
+
+    def test_insert_and_search_empty_prefix(self):
+        self.trie.insert("apple", ("file1.txt", 1, 1))
+        self.trie.insert("banana", ("file1.txt", 1, 2))
+        self.trie.insert("cherry", ("file1.txt", 2, 1))
+        self.trie.insert("apple", ("file2.txt", 1, 1))
+
+        locations_empty = self.trie.search("")
+        self.assertEqual(locations_empty, {})
+
+    def test_search_nonexistent_prefix(self):
+        self.trie.insert("apple", ("file1.txt", 1, 1))
+        self.trie.insert("banana", ("file1.txt", 1, 2))
+
+        locations_nonexistent = self.trie.search("grape")
+        self.assertEqual(locations_nonexistent, {})
+
+
+if __name__ == '__main__':
+    unittest.main()
