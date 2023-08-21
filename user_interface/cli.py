a314d3282bd267aa48bc01c538dcf818b5ad5d21
Ruty Rosenberg <80812161+rurosenb@users.noreply.github.com>
2023-08-21 22:58:13 +0300

    Update cli.py

--- user_interface/cli.py
+++ user_interface/cli.py
@@ -1,6 +1,10 @@
 import argparse
 import re
 
+from trie_processor import load_database_process
+from search_complitions import search_completions
+
+
 # Define your search engine logic here
 def clean_text(text):
     # Convert to lowercase, remove punctuation, and replace multiple spaces with a single space
@@ -9,32 +13,34 @@ def clean_text(text):
     text = re.sub(r'\s+', ' ', text).strip()
     return text
 
-# Define your search engine logic here
+
 def search_engine_logic():
-    # Load and analyze text files
-    print("Loading the file and preparing the system...")
-
-    #Rivki's function
-
-    # Get user input
     user_input = input("The system is ready. Enter your text:\n")
-
-    # Perform completion logic and return offers
-    print("Completing logic and returning offers for:", clean_text(user_input).split())
-
-    print("Here are 5 suggestions")
-
-    #Avigail's function
+    return user_input
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Simple Search Engine CLI")
+    parser = argparse.ArgumentParser(description="Search Engine CLI Google autocomplete project")
     parser.add_argument("-l", "--load", action="store_true", help="Load and analyze text files")
-
     args = parser.parse_args()
-
     if args.load:
-        search_engine_logic()
+        print("Loading the file and preparing the system...")
+        Trie = load_database_process()
+        prefix = search_engine_logic()
+        while True:
+            if prefix.endswith('#'):
+                # break
+                prefix = search_engine_logic()
+
+            prefix = clean_text(prefix)
+            print("Completing logic and returning offers for:", prefix)
+            sentences_list = search_completions(prefix, Trie)
+            print("Here are 5 suggestions")
+            if sentences_list:
+                for sentence in sentences_list:
+                    print(sentence)
+            print("You can continue typing or type '#' to start over.\n")
+            prefix = prefix + " " + input(prefix + " ")
 
 
 if __name__ == "__main__":
