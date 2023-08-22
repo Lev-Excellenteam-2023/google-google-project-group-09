0bc09244d94f206801bbb12e1d7257bc8965bc12
rivkaadler1 <adlerrivka66@gmail.com>
2023-08-22 10:55:48 +0300

    move find_intersection function from find_sentence.py(deleted) to search_completions.py

--- sentence_completion/search_completions.py
+++ sentence_completion/search_completions.py
@@ -26,6 +26,30 @@ def search_clause(search_results: list):
     """
     pass
 
+def find_intersection(dict1, dict2):
+    intersection_dictionary = {}
+
+    for filename in dict1:
+        if filename in dict2:
+            locations_of_word1 = dict1[filename]
+            locations_of_word2 = dict2[filename]
+
+            intersection_locations = []
+
+            for location1 in locations_of_word1:
+                line1, offset1 = location1
+
+                for location2 in locations_of_word2:
+                    line2, offset2 = location2
+
+                    if line1 == line2 and offset1 + 1 == offset2:
+                        intersection_locations.append(location2)
+
+            if intersection_locations:
+                intersection_dictionary[filename] = intersection_locations
+
+    return intersection_dictionary
+
 
 def search_completions(searching: str, trie: trie_ds.Trie, max_results=5):
     """
